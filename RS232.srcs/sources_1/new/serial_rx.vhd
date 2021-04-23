6b80aad8c364aaf258c89ab2d0569bd252db1a04
Arkadiusz Górecki <arkadiusz.gorecki52@gmail.com>
2021-04-24 01:46:52 +0200

    Add main process with states

--- RS232.srcs/sources_1/new/serial_rx.vhd
+++ RS232.srcs/sources_1/new/serial_rx.vhd
@@ -12,11 +12,11 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity SERIAL_RX is
     generic (
-        F_ZEGARA : natural; -- czêstotliwoœc próbkowania
-        L_BODOW : natural; -- liczba bodów/baud rate?
-        B_SLOWA : natural; -- d³ugoœc s³owa?
-        B_PARZYSTOSCI : natural;
-        B_STOPOW : natural;
+        CLOCK_F : natural := 20_000_000;
+        BAUDRATE : natural := 5_000_000;
+        DATA_L : natural; -- (5-8) data length, how many bits
+        PARITY_L : natural; -- 0 or 1, parity bits length, informs whether we should read the parity bit after data transmission
+        STOP_L : natural; -- 1 or 2, stop bits length, how many ending bits after data transmission
         N_RX : boolean;
         N_SLOWO : boolean
     );
@@ -24,15 +24,57 @@ entity SERIAL_RX is
         R : in std_logic;
         C : in std_logic;
         RX : in std_logic;
-        SLOWO : out std_logic_vector (B_SLOWA - 1 downto 0);
-        GOTOWE : out std_logic;
-        BLAD :out std_logic
+        DATA : out std_logic_vector (DATA_L - 1 downto 0);
+        READY : out std_logic;
+        ERR :out std_logic
     );
 end SERIAL_RX;
 
 architecture Behavioral of SERIAL_RX is
-
+    type state is (WAITING, START, READ, PARITY, STOP);
 begin
 
+    process(R, C) is
+        constant T : natural := CLOCK_F / BAUDRATE; -- liczba taktów zegara przypadaj¹ca na jeden bit danej (jeden bod)
+        variable currT : natural := 0;
+        variable curr_state : state := WAITING;
+        variable data_read : natural := 0; -- how many Rx data bits already read
+        variable stop_read : natural := 0;
+    begin
+        -- narpierw trzeba zrobiæ offset T/2 ¿eby po œrodku szczytywaæ a dopiero potem normalnie wystartowaæ z okresami co T
+        case curr_state is
+            when WAITING =>
+                if RX = '1' then
+                    curr_state := START;
+                end if;
+            when START =>
+                currT := currT + 1;
 
+                if currT >= T/2 then
+                    curr_state := READ;
+                    currT := 0;
+                end if;
+            when READ =>
+                currT := currT + 1;
+
+                if currT >= T then
+                    DATA(data_read) <= RX;
+                    data_read := data_read + 1;
+                    currT := 0;
+                    if data_read >= DATA_L then
+                        case PARITY_L is
+                            when 0 => curr_state := STOP; -- do not check parity bit
+                            when 1 => curr_state := PARITY;
+                        end case;                            
+                    end if;     
+                end if;           
+            when PARITY => -- !!!niedokoñczone
+                currT := currT + 1;
+                if currT >= T then
+                    currT := 0;
+                end if;  
+            when STOP =>
+        end case;
+                 
+    end process;
 end Behavioral;
